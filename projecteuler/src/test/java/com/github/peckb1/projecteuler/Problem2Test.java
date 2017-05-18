bcc0227910a7834a9d8b48bd536b9b8031ad125c
Brian Peck <Catbert321@gmail.com>
2017-05-17 21:13:01 -0700

    Problem 2 for Project Euler (#49)

---
+++ projecteuler/src/test/java/com/github/peckb1/projecteuler/Problem2Test.java
@@ -0,0 +1,22 @@
+package com.github.peckb1.projecteuler;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+public class Problem2Test {
+
+    private Problem2 problem2;
+
+    @Before
+    public void setUp() throws Exception {
+        this.problem2 = new Problem2();
+    }
+
+    @Test
+    public void testProblem2() throws Exception {
+        Assert.assertEquals(BigInteger.valueOf(4613732), this.problem2.fibEvenSum(4_000_000));
+    }
+}
