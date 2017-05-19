58729eecaeb0e43b6aafa5ee528c25338f1e5c8f
Brian Peck <Catbert321@gmail.com>
2017-05-18 21:04:56 -0700

    Problem 3 for Project Euler (#50)

---
+++ projecteuler/src/test/java/com/github/peckb1/projecteuler/Problem3Test.java
@@ -0,0 +1,22 @@
+package com.github.peckb1.projecteuler;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Problem3Test {
+
+    private Problem3 problem3;
+
+    @Before
+    public void setUp() throws Exception {
+        this.problem3 = new Problem3();
+    }
+
+    @Test
+    public void testProblem3() throws Exception {
+        Integer largestFactor = this.problem3.primeFactors(600_851_475_143L).stream().reduce(1, Math::max);
+        Assert.assertEquals(new Integer(6857), largestFactor);
+    }
+
+}
