344c1189a731e2a3a6e1e8d0d9e0616e86da9ff0
Brian Peck <catbert321@gmail.com>
2017-05-23 19:40:00 -0700

    Update project euler packages

--- projecteuler/src/main/java/com/github/peckb1/projecteuler/p001to010/Problem1.java
+++ projecteuler/src/main/java/com/github/peckb1/projecteuler/p001to010/Problem1.java
@@ -1,4 +1,4 @@
-package com.github.peckb1.projecteuler;
+package com.github.peckb1.projecteuler.p001to010;
 
 import java.math.BigInteger;
 import java.util.Arrays;
--- projecteuler/src/main/java/com/github/peckb1/projecteuler/p001to010/Problem4.java
+++ projecteuler/src/main/java/com/github/peckb1/projecteuler/p001to010/Problem4.java
@@ -1,4 +1,4 @@
-package com.github.peckb1.projecteuler;
+package com.github.peckb1.projecteuler.p001to010;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -63,32 +63,6 @@ public class Problem4 {
         }
         divisors.add(n);
         return divisors;
-    }
-
-    private List<Integer> primeFactors(int n) {
-        List<Integer> factors = new ArrayList<>();
-        while (n % 2 == 0) {
-            factors.add(2);
-            n /= 2;
-        }
-
-        // n must be odd at this point. So we can
-        // skip one element (Note i = i + 2)
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
-            // While i divides n, save i and divide n
-            while (n % i == 0) {
-                factors.add(i);
-                n /= i;
-            }
-        }
-
-        // This condition is to handle the case when
-        // n is a prime number greater than 2
-        if (n > 2) {
-            factors.add(n);
-        }
-
-        return factors;
     }
 
 }
